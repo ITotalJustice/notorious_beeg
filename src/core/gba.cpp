ITotalJustice/notorious_beeg

commit f5e92fd29aedb812210775557ce795bd45d4bd3a

implement rtc
works in pokemon, that's all that matters

src/core/CMakeLists.txt
@@ -20,6 +20,7 @@ else ()
         bios.cpp
         bios_hle.cpp
         scheduler.cpp
+        rtc.cpp
 
         backup/backup.cpp
         backup/eeprom.cpp

src/core/gba.cpp
@@ -209,7 +209,7 @@ auto Gba::render_mode(std::span<u16> pixels, u8 mode, u8 layer) -> u8
 namespace state {
 
 constexpr auto MAGIC = 0xFACADE;
-constexpr auto VERSION = 0x1;
+constexpr auto VERSION = 0x2;
 constexpr auto SIZE = sizeof(State);
 
 } // namespace state
@@ -247,6 +247,8 @@ auto Gba::loadstate(const State& state) -> bool
     this->timer[2] = state.timer[2];
     this->timer[3] = state.timer[3];
     this->backup = state.backup;
+    this->gpio = state.gpio;
+
     mem::setup_tables(*this);
     scheduler::on_loadstate(*this);
 
@@ -274,6 +276,7 @@ auto Gba::savestate(State& state) const -> bool
     state.timer[2] = this->timer[2];
     state.timer[3] = this->timer[3];
     state.backup = this->backup;
+    state.gpio = this->gpio;
 
     return true;
 }

src/core/gba.hpp
@@ -11,6 +11,7 @@
 #include "timer.hpp"
 #include "scheduler.hpp"
 #include "backup/backup.hpp"
+#include "gpio.hpp"
 #include "fwd.hpp"
 #include <span>
 #include <string_view>
@@ -52,6 +53,7 @@ struct Gba
     dma::Channel dma[4];
     timer::Timer timer[4];
     backup::Backup backup;
+    gpio::Gpio gpio;
 
     // these are not not part of the mem struct because we do not
     // want to savestate this :harold:
@@ -99,7 +101,7 @@ struct Gba
 struct State
 {
     u32 magic; // 0xFACADE
-    u32 version; // 0x1
+    u32 version; // 0x2
     u32 size; // size of state struct
     u32 crc; // crc of game
 
@@ -111,6 +113,7 @@ struct State
     dma::Channel dma[4];
     timer::Timer timer[4];
     backup::Backup backup;
+    gpio::Gpio gpio;
 };
 
 struct Header

src/core/gpio.hpp
@@ -1,0 +1,21 @@
+// Copyright 2022 TotalJustice.
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#include "fwd.hpp"
+#include "rtc.hpp"
+
+namespace gba::gpio {
+
+struct Gpio
+{
+    rtc::Rtc rtc;
+
+    u8 data{}; // 4 bit
+    u8 read_mask{0b0000};
+    u8 write_mask{0b111};
+    bool rw{false};
+};
+
+} // namespace gba::gpio

src/core/rtc.cpp
@@ -1,0 +1,268 @@
+// Copyright 2022 TotalJustice.
+// SPDX-License-Identifier: GPL-3.0-only
+
+// SOURCE:
+// https://problemkaputt.de/gbatek-ds-real-time-clock-rtc.htm
+// https://problemkaputt.de/gbatek-gba-cart-real-time-clock-rtc.htm
+// https://problemkaputt.de/gbatek-gba-cart-i-o-port-gpio.htm
+// https://github.com/pret/pokeemerald/blob/677b4fc394516deab5b5c86c94a2a1443cb52151/src/rtc.c
+// https://github.com/pret/pokeemerald/blob/78b0c207388d8915c7fb4a509334abbeb4680d0d/src/siirtc.c
+// https://beanmachine.alt.icu/post/rtc/
+
+#include "rtc.hpp"
+#include "gba.hpp"
+#include "bit.hpp"
+#include <cstdio>
+#include <cassert>
+#include <bit>
+#include <utility>
+#include <ctime>
+
+namespace gba::rtc {
+namespace {
+
+constexpr auto COMMAND_MAGIC = 0b0110;
+
+constexpr u8 COMMAND_LENGTH[]
+{
+    1 * 8,
+    1 * 8,
+    7 * 8,
+    3 * 8,
+    3 * 8,
+    3 * 8,
+    0 * 8,
+    0 * 8,
+};
+
+constexpr auto bcd(u8 value) -> u64
+{
+    // bcd works like this:
+    // the upper nibble are the tens (10, 20, 30).
+    // the lower nibble are the ones (1...9).
+    // so, the biggest number that can be represented is 99 (0b1001'1001)
+    assert(value <= 99 && "max value for BCD, what happens here? (mask?)");
+
+    const auto tens = value / 10;
+    const auto ones = value % 10;
+
+    return (tens << 4) | ones;
+}
+
+} // namespace
+
+auto Rtc::write(Gba& gba, u32 addr, u8 value) -> void
+{
+    const auto SCK = bit::is_set<0>(value);
+    const auto SIO = bit::is_set<1>(value);
+    const auto CS = bit::is_set<2>(value);
+
+    switch (this->state)
+    {
+        case State::init1: {
+            assert(!CS && SCK && "wrong command sequence!");
+            if (!CS && SCK)
+            {
+                this->bits = 0;
+                this->bit_counter = 0;
+                this->state = State::init2;
+            }
+        } break;
+
+        case State::init2: {
+            if (CS && SCK)
+            {
+                this->state = State::command;
+            }
+        } break;
+
+        // CS has to remian 1 for the whole transfer
+        // when SCK=0, write a single bit to pending bit
+        // when SCK=1, write the pending bit into bits
+        case State::command:
+            if (!CS)
+            {
+                // error (what happens here)
+                assert(CS && "CS went low, not sure why");
+            }
+            else if (SCK == 0)
+            {
+                this->pending_bit = SIO;
+            }
+            else if (SCK == 1)
+            {
+                // "read" bit into bits (aka, write the data)
+                this->bits |= static_cast<u64>(this->pending_bit) << this->bit_counter;
+                this->bit_counter++;
+
+                // check if we have a full byte
+                if (this->bit_counter == 8)
+                {
+                    // bits 4-7 must be 0b0110, if not then byte swap
+                    if (COMMAND_MAGIC != bit::get_range<4, 7>(this->bits))
+                    {
+                        assert(COMMAND_MAGIC == (bit::get_range<0, 4>(this->bits)) && "invalid command magic");
+                        this->bits = bit::reverse2<u8>(this->bits);
+                    }
+
+                    // if bit0 = 0, this is a write command
+                    // if bit0 = 1, this is a read command
+                    if (bit::is_set<0>(this->bits))
+                    {
+                        this->state = State::read;
+                    }
+                    else
+                    {
+                        this->state = State::write;
+                    }
+
+                    this->command = static_cast<Command>(bit::get_range<1, 3>(this->bits));
+                    this->bits = 0;
+                    this->bit_counter = 0;
+
+                    switch (this->command)
+                    {
+                        case Command::reset:
+                            this->control = 0;
+                            this->state = State::init1;
+                            break;
+
+                        case Command::control:
+                            if (this->state == State::read)
+                            {
+                                this->bits = this->control;
+                            }
+                            break;
+
+                        case Command::date:
+                            if (this->state == State::read)
+                            {
+                                std::time_t timer;
+                                std::time(&timer);
+                                const auto tm = std::localtime(&timer);
+
+                                // year range is 2000-2099
+                                // tm represents the year since 1900
+                                // so for 2022, the value will be 122
+                                this->bits |= bcd(tm->tm_year % 100) << 0;
+                                // month range is 1-12
+                                // tm represents the month as 0-11
+                                this->bits |= bcd(tm->tm_mon + 1) << 8;
+                                // day range is 1-31
+                                // this is the same as rtc
+                                this->bits |= bcd(tm->tm_mday) << 16;
+                                // day range is 0-11
+                                // this is the same as rtc
+                                this->bits |= bcd(tm->tm_wday) << 24;
+
+                                this->bits |= bcd(tm->tm_hour) << 32;
+                                this->bits |= bcd(tm->tm_min) << 40;
+                                this->bits |= bcd(tm->tm_sec) << 48;
+                            }
+                            break;
+
+                        case Command::time:
+                            if (this->state == State::read)
+                            {
+                                std::time_t timer;
+                                std::time(&timer);
+                                const auto tm = std::localtime(&timer);
+
+                                this->bits |= bcd(tm->tm_hour) << 0;
+                                this->bits |= bcd(tm->tm_min) << 8;
+                                this->bits |= bcd(tm->tm_sec) << 16;
+                            }
+                            break;
+
+                        case Command::alarm1: // (dsi only?)
+                            assert(!"invalid command! alarm1");
+                            break;
+
+                        case Command::alarm2: // (dsi only?)
+                            assert(!"invalid command! alarm1");
+                            break;
+
+                        case Command::irq:
+                            assert(!"unhandled command [irq]");
+                            break;
+
+                        case Command::unused:
+                            assert(!"invalid command! unused");
+                            break;
+                    }
+                }
+            }
+            break;
+
+        case State::read:
+            if (!CS)
+            {
+                this->state = State::init1;
+                assert(bit_counter == COMMAND_LENGTH[std::to_underlying(this->command)] && "this will break read as not the full byte is read");
+            }
+            else if (SCK == 0)
+            {
+                // what do i do here????????????
+                // this->pending_bit = bit::is_set(this->bits, this->bit_counter);
+            }
+            else if (SCK == 1)
+            {
+                this->pending_bit = bit::is_set(this->bits, this->bit_counter);
+                gba.gpio.data = bit::set<1>(gba.gpio.data, this->pending_bit);
+                this->bit_counter++;
+
+                if (bit_counter == COMMAND_LENGTH[std::to_underlying(this->command)])
+                {
+                    this->state = State::init1;
+                }
+            }
+            break;
+
+        case State::write:
+            if (!CS)
+            {
+                this->state = State::init1;
+                assert(bit_counter == COMMAND_LENGTH[std::to_underlying(this->command)] && "this will break writes as not the full byte is written");
+            }
+            else if (SCK == 0)
+            {
+                this->pending_bit = SIO;
+            }
+            else if (SCK == 1)
+            {
+                // "read" bit into bits (aka, write the data)
+                this->bits |= static_cast<u64>(this->pending_bit) << this->bit_counter;
+                this->bit_counter++;
+
+                // check if we have a full byte
+                if (bit_counter >= COMMAND_LENGTH[std::to_underlying(this->command)])
+                {
+                    this->state = State::init1;
+
+                    switch (this->command)
+                    {
+                        case Command::control: {
+                            auto data = this->bits;
+                            data = bit::unset<0>(data); // unused
+                            data = bit::unset<2>(data); // unused
+                            data = bit::unset<4>(data); // unused
+                            data = bit::unset<7>(data); // r
+                            this->control = data;
+                            std::printf("control is now: 0x%02X\n", this->control);
+                        } break;
+
+                        case Command::reset: assert(!"[WRITE] Command::reset"); break;
+                        case Command::date: assert(!"[WRITE] Command::date"); break;
+                        case Command::time: assert(!"[WRITE] Command::time"); break;
+                        case Command::alarm1: assert(!"[WRITE] Command::alarm1"); break;
+                        case Command::alarm2: assert(!"[WRITE] Command::alarm2"); break;
+                        case Command::irq: assert(!"[WRITE] Command::irq"); break;
+                        case Command::unused: assert(!"[WRITE] Command::unused"); break;
+                    }
+                }
+            }
+            break;
+    }
+}
+
+} // namespace gba::rtc

src/core/rtc.hpp
@@ -1,0 +1,52 @@
+// Copyright 2022 TotalJustice.
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#include "fwd.hpp"
+
+namespace gba::rtc {
+
+enum class State : u8
+{
+    // waits until CS=0 and SCK=1
+    init1,
+
+    // waits until CS=1 and SCK=1 (CS rises)
+    init2,
+
+    // waits until 8 bits have been transfered
+    command,
+
+    // idk about these yet
+    read,
+    write,
+};
+
+enum class Command : u8
+{
+    reset = 0,
+    control = 1,
+    date = 2,
+    time = 3,
+    alarm1 = 4,
+    alarm2 = 5,
+    irq = 6,
+    unused = 7,
+};
+
+struct Rtc
+{
+    u64 bits; // bits shifted in / out
+    u8 bit_counter; // bit position
+    bool pending_bit; // pending bit that's been written
+
+    u8 control{0b0100'0000}; // enable 24h, thats it
+
+    State state{State::init1};
+    Command command;
+
+    auto write(Gba& gba, u32 addr, u8 value) -> void;
+};
+
+} // namespace gba::rtc

src/core/bit.hpp
@@ -272,4 +272,62 @@ static_assert(
     "bit::set is broken!"
 );
 
+// reverses the bits
+template<IntV T> [[nodiscard]]
+constexpr auto reverse(const T data)
+{
+    constexpr auto bit_width = sizeof(T) * 8;
+    T output{};
+
+    for (std::size_t i = 0; i < bit_width; i++)
+    {
+        output |= bit::is_set(data, bit_width - 1 - i) << i;
+    }
+
+    return output;
+}
+
+// same as above but without a loop (recursion is compile-time only)
+template<IntV T, u8 Counter = 0> [[nodiscard]]
+constexpr auto reverse2(const T data, T output = 0)
+{
+    constexpr auto bit_width = sizeof(T) * 8;
+
+    if constexpr (Counter == bit_width)
+    {
+        return output;
+    }
+    else
+    {
+        const auto result = bit::is_set<bit_width - 1 - Counter>(data);
+        output = bit::set<Counter>(output, result);
+        return reverse2<T, Counter + 1>(data, output);
+    }
+}
+
+static_assert(
+    bit::reverse<u8>(0b0000'0001) == 0b1000'0000 &&
+    bit::reverse<u8>(0b1000'0000) == 0b0000'0001 &&
+
+    bit::reverse<u8>(0b0000'1111) == 0b1111'0000 &&
+    bit::reverse<u8>(0b1111'0000) == 0b0000'1111 &&
+
+    bit::reverse<u8>(0b0000'0110) == 0b0110'0000 &&
+    bit::reverse<u8>(0b0110'0000) == 0b0000'0110,
+
+    "bit::reverse is broken!"
+);
+
+static_assert(
+    bit::reverse2<u8>(0b0000'0001) == 0b1000'0000 &&
+    bit::reverse2<u8>(0b1000'0000) == 0b0000'0001 &&
+
+    bit::reverse2<u8>(0b0000'1111) == 0b1111'0000 &&
+    bit::reverse2<u8>(0b1111'0000) == 0b0000'1111 &&
+
+    bit::reverse2<u8>(0b0000'0110) == 0b0110'0000 &&
+    bit::reverse2<u8>(0b0110'0000) == 0b0000'0110,
+
+    "bit::reverse2 is broken!"
+);
 } // namespace bit

src/core/mem.cpp
@@ -435,7 +435,7 @@ constexpr auto write_io16(Gba& gba, const u32 addr, const u16 value) -> void
             const auto old_value = REG_WSCNT;
             const auto new_value = value;
 
-            std::printf("[IO_WSCNT] 0x%04X\n", value);
+            // std::printf("[IO_WSCNT] 0x%04X\n", value);
             REG_WSCNT = value;
 
             if (old_value != new_value)
@@ -806,6 +806,70 @@ constexpr auto write_oam_region(Gba& gba, const u32 addr, const T value) -> void
     }
 }
 
+template<typename T>
+constexpr auto read_gpio(Gba& gba, const u32 addr) -> T
+{
+    assert(gba.gpio.rw && "this handler should only be called when gpio is rw");
+
+    switch (addr)
+    {
+        case 0x80000C4: // I/O Port Data (rw or W)
+            return gba.gpio.data & gba.gpio.read_mask;
+
+        case 0x80000C6: // I/O Port Direction (rw or W)
+            return gba.gpio.write_mask; // remember we modify the rmask
+
+        case 0x80000C8: // I/O Port Control (rw or W)
+            return gba.gpio.rw;
+
+        default:
+            return read_array<T>(gba.rom, ROM_MASK, addr);
+    }
+}
+
+template<typename T>
+constexpr auto write_gpio(Gba& gba, const u32 addr, const T value)
+{
+    switch (addr)
+    {
+        case 0x80000C4: // I/O Port Data (rw or W)
+            // std::printf("[GPIO] data: 0x%02X\n", value);
+            gba.gpio.data = value & gba.gpio.write_mask;
+            gba.gpio.rtc.write(gba, addr, value & gba.gpio.write_mask);
+            // gba.gpio.data = value & gba.gpio.write_mask;
+            break;
+
+        case 0x80000C6: // I/O Port Direction (rw or W)
+            // std::printf("[GPIO] direction: 0x%02X\n", value);
+            // the direction port acts as a mask for r/w bits
+            // bitX = 0, read only (in)
+            // bitX = 1, write only (out)
+            gba.gpio.read_mask = bit::get_range<0, 4>(~value);
+            gba.gpio.write_mask = bit::get_range<0, 4>(value);
+            break;
+
+        case 0x80000C8: // I/O Port Control (rw or W)
+            gba.gpio.rw = bit::is_set<0>(value);
+
+            std::printf("[GPIO] control: %s\n", gba.gpio.rw ? "rw" : "w only");
+            if (gba.gpio.rw)
+            {
+                // for speed, unmap the rom from [0x8]
+                // this will cause the function ptr handler to be called instead
+                // which will handle the reads to gpio and rom
+                MEM.rmap_32[0x8] = MEM.rmap_16[0x8] = MEM.rmap_8[0x8] = {};
+            }
+            else
+            {
+                // gpio is now write only
+                // remap rom array for faster reads
+                std::printf("unammped rom handler\n");
+                MEM.rmap_32[0x8] = MEM.rmap_16[0x8] = MEM.rmap_8[0x8] = {gba.rom, ROM_MASK};
+            }
+            break;
+    }
+}
+
 // https://github.com/mgba-emu/mgba/issues/743 (thanks endrift)
 [[nodiscard]]
 constexpr auto is_vram_dma_overflow_bug(Gba& gba, const u32 addr)
@@ -1023,7 +1087,7 @@ constexpr ReadFunction<T> READ_FUNCTION[0x10] =
     /*[0x5] =*/ read_pram_region<T>,
     /*[0x6] =*/ read_vram_region<T>,
     /*[0x7] =*/ read_oam_region<T>,
-    /*[0x8] =*/ openbus, // todo: add this
+    /*[0x8] =*/ read_gpio, // called when gpio is rw (w only default)
     /*[0x9] =*/ openbus, // todo: add this
     /*[0xA] =*/ openbus, // todo: add this
     /*[0xB] =*/ openbus, // todo: add this
@@ -1044,7 +1108,7 @@ constexpr WriteFunction<T> WRITE_FUNCTION[0x10] =
     /*[0x5] =*/ write_pram_region<T>,
     /*[0x6] =*/ write_vram_region<T>,
     /*[0x7] =*/ write_oam_region<T>,
-    /*[0x8] =*/ empty_write,
+    /*[0x8] =*/ write_gpio,
     /*[0x9] =*/ empty_write,
     /*[0xA] =*/ empty_write,
     /*[0xB] =*/ empty_write,
@@ -1081,6 +1145,12 @@ auto setup_tables(Gba& gba) -> void
     MEM.wmap_16[0x3] = {gba.mem.iwram, IWRAM_MASK};
     // MEM.wmap_16[0x5] = {gba.mem.pram, PRAM_MASK};
     // MEM.wmap_16[0x7] = {gba.mem.oam, OAM_MASK};
+
+    // unmap rom array from 0x8 and let the func fallback handle it
+    if (gba.gpio.rw)
+    {
+        MEM.rmap_16[0x8] = {};
+    }
 
     // this will be handled by the function handlers
     if (gba.backup.type == backup::Type::EEPROM)

commit b46884786d59ed4d8ac58b7e81243ba50ddb4c18

fix very bloated binary size due to rtc commit
caused in commit https://github.com/ITotalJustice/notorious_beeg/commit/f5e92fd29aedb812210775557ce795bd45d4bd3a
specifically https://github.com/ITotalJustice/notorious_beeg/blob/f5e92fd29aedb812210775557ce795bd45d4bd3a/src/core/gpio.hpp#L17

this is due to the gba core being static.

src/core/CMakeLists.txt
@@ -20,6 +20,7 @@ else ()
         bios.cpp
         bios_hle.cpp
         scheduler.cpp
+        gpio.cpp
         rtc.cpp
 
         backup/backup.cpp

src/core/gba.cpp
@@ -78,9 +78,10 @@ auto Gba::reset() -> void
 
     scheduler::reset(*this);
     mem::reset(*this, skip_bios); // this needed to be before arm::reset because memtables
-    arm7tdmi::reset(*this, skip_bios);
     ppu::reset(*this, skip_bios);
     apu::reset(*this, skip_bios);
+    gpio::reset(*this, skip_bios);
+    arm7tdmi::reset(*this, skip_bios);
 }
 
 auto Gba::loadrom(std::span<const u8> new_rom) -> bool

src/core/gba.hpp
@@ -59,7 +59,7 @@ struct Gba
     // want to savestate this :harold:
 
     // 16kb, 32-bus
-    alignas(u32) u8 bios[1024 * 16]{};
+    alignas(u32) u8 bios[1024 * 16];
     // 32mb(max), 16-bus
     alignas(u32) u8 rom[0x2000000];
 

src/core/gpio.cpp
@@ -1,0 +1,18 @@
+// Copyright 2022 TotalJustice.
+// SPDX-License-Identifier: GPL-3.0-only
+#include "gpio.hpp"
+#include "gba.hpp"
+
+namespace gba::gpio {
+
+auto reset(Gba& gba, [[maybe_unused]] bool skip_bios) -> void
+{
+    gba.gpio.rtc.init();
+    gba.gpio.data = 0;
+    gba.gpio.read_mask = 0b0000;
+    gba.gpio.write_mask = 0b1111;
+    gba.gpio.rw = false;
+}
+
+// 1.4 MiB (1,500,920)
+} // namespace gba::gpio

src/core/gpio.hpp
@@ -12,10 +12,12 @@ struct Gpio
 {
     rtc::Rtc rtc;
 
-    u8 data{}; // 4 bit
-    u8 read_mask{0b0000};
-    u8 write_mask{0b111};
-    bool rw{false};
+    u8 data; // 4 bit
+    u8 read_mask;
+    u8 write_mask;
+    bool rw;
 };
+
+auto reset(Gba& gba, bool skip_bios) -> void;
 
 } // namespace gba::gpio

src/core/mem.hpp
@@ -29,30 +29,30 @@ struct WriteArray
 // this will remove casting in most common cases.
 struct Mem
 {
-    ReadArray rmap_8[16]{};
-    ReadArray rmap_16[16]{};
-    ReadArray rmap_32[16]{};
-    WriteArray wmap_8[16]{};
-    WriteArray wmap_16[16]{};
-    WriteArray wmap_32[16]{};
+    ReadArray rmap_8[16];
+    ReadArray rmap_16[16];
+    ReadArray rmap_32[16];
+    WriteArray wmap_8[16];
+    WriteArray wmap_16[16];
+    WriteArray wmap_32[16];
 
     // 256kb, 16-bit bus
-    alignas(u32) u8 ewram[1024 * 256]{};
+    alignas(u32) u8 ewram[1024 * 256];
 
     // 32kb, 32-bit bus
-    alignas(u32) u8 iwram[1024 * 32]{};
+    alignas(u32) u8 iwram[1024 * 32];
 
     // 1kb, 16-bit
-    alignas(u32) u8 pram[1024 * 1]{};
+    alignas(u32) u8 pram[1024 * 1];
 
     // 96kb, 16-bit bus
-    alignas(u32) u8 vram[1024 * 96]{};
+    alignas(u32) u8 vram[1024 * 96];
 
     // 1kb, 32-bit
-    alignas(u64) u8 oam[1024 * 1]{};
+    alignas(u64) u8 oam[1024 * 1];
 
     // 1kb, 16/32-bit
-    alignas(u32) u8 io[1024 * 1]{};
+    alignas(u32) u8 io[1024 * 1];
 
     // internal memory control
     alignas(u32) u8 imc[4];

src/core/rtc.cpp
@@ -51,6 +51,18 @@ constexpr auto bcd(u8 value) -> u64
 
 } // namespace
 
+auto Rtc::init() -> void
+{
+    this->bits = 0; // bits shifted in / out
+    this->bit_counter = 0; // bit position
+    this->pending_bit = false; // pending bit that's been written
+
+    this->control = 0b0100'0000; // enable 24h, thats it
+
+    this->state = State::init1;
+    this->command = Command::unused;
+}
+
 auto Rtc::write(Gba& gba, u32 addr, u8 value) -> void
 {
     const auto SCK = bit::is_set<0>(value);

src/core/rtc.hpp
@@ -41,11 +41,12 @@ struct Rtc
     u8 bit_counter; // bit position
     bool pending_bit; // pending bit that's been written
 
-    u8 control{0b0100'0000}; // enable 24h, thats it
+    u8 control; // enable 12/24h, thats it
 
-    State state{State::init1};
+    State state;
     Command command;
 
+    auto init() -> void;
     auto write(Gba& gba, u32 addr, u8 value) -> void;
 };
 

src/core/single.cpp
@@ -17,6 +17,7 @@
     #include "bios.cpp"
     #include "bios_hle.cpp"
     #include "scheduler.cpp"
+    #include "gpio.cpp"
     #include "rtc.cpp"
 
     #include "backup/backup.cpp"